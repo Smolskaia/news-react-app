1b10cded2694c3af07731ffb6078bb614449074b
Anna Smolskaya <anna31rus@mail.ru>
2024-08-26 21:51:16 +0500

    News app created

---
+++ src/helpers/formatDate.js
@@ -0,0 +1,13 @@
+export const formatDate = (date) => {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+};
+
+
+// const date = new Date("2029-09-26");
+// console.log(formatDate(date));
